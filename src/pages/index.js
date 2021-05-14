c357f2f33e81bc0fe11dd57da7e7675e1993963a
Mina Bui <hello@minabui.com>
2021-05-13 18:29:23 -0700

    Working on homepage formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,52 +16,29 @@ const IndexPage = () => (
       I like to build responsive, interactive websites and web applications.
     </p>
       <p>
-        <Link to="/page-2/"><button>Hire Me</button></Link>
+        <Link to="/contact/"><button>Contact Me</button></Link>
       </p>
     </div>
-
-    <hr />
 
     <div class="section-2">
       <h3>About Me</h3>
       <p>
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
-    </p>
+      </p>
       <p>
-        <Link to="/page-2/"><button>Read More About Me</button></Link>
+        <Link to="/about/"><button>Learn More</button></Link>
       </p>
     </div>
-
-    <hr />
 
     <div class="section-3">
       <h3>My Projects</h3>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi. architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione.
     </p>
       <p>
-        <Link to="/page-2/"><button>See All My Projects</button></Link>
+        <Link to="/projects/"><button>See All Projects</button></Link>
       </p>
     </div>
-
-    <hr />
-
-    <div class="section-4">
-      <StaticImage
-        src="../images/gatsby-astronaut.png"
-        width={300}
-        quality={95}
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt="A Gatsby astronaut"
-      />
-      <p>
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-      </p>
-
-    </div>
-
-    <hr />
 
   </Layout>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,6 +10,15 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <div class="section-4">
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+      />
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
