d0bf52ac7d1dbd4c276cf1ae6e0319b56627dbb1
Mina Bui <hello@minabui.com>
2021-06-07 20:08:58 -0700

    Working on front-page indefinitely...

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header class="header">
 
     <div class="header-flex">
@@ -11,7 +11,7 @@ const Header = ({ siteTitle }) => (
         <Link to="/">
           <StaticImage
             src="../images/mina-bui_logo_symbol.png"
-            width={75}
+            width={50}
             quality={95}
             formats={["AUTO", "WEBP", "AVIF"]}
             alt="Mina Bui's Logo"
@@ -22,30 +22,16 @@ const Header = ({ siteTitle }) => (
       <div class="header-nav">
         <nav>
           <ul>
-            <li><Link to="/about/">About</Link></li>
             <li><Link to="/projects/">Projects</Link></li>
+            <li><Link to="/about/">About</Link></li>
             <li><Link to="/contact/">Contact</Link></li>
           </ul>
         </nav>
-      </div>
-
-      <div class="header-hire">
-        <ul>
-          <li><Link to="mailto:hello@minabui.com?subject=Hello from your site!" id="header-hire-btn">Hire Me</Link></li>
-        </ul>
       </div>
 
     </div>
 
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -12,8 +13,7 @@ const IndexPage = () => (
     <div class="section-1">
       <h1 class="section-1_h2">Mina Bui</h1>
       <p>
-        Front-End Developer and Web Designer based in Vancouver. <br />
-      I build responsive, interactive websites and web applications. <Link to="/about/">Learn more.</Link>
+        Front-End Developer with a love for web design, based in Vancouver.
       </p>
       <p>
         <Link to="/contact/"><button>Contact Me</button></Link>
@@ -21,23 +21,114 @@ const IndexPage = () => (
     </div>
 
     <div class="section-2">
-      <h3>About Me</h3>
+      <h2>About Me</h2>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+        Vietnamese-Canadian living in British Columbia who loves all things design and spends an unusual amount of time on the internet. Has a particular interest in user friendly, responsive interfaces.
       </p>
+
       <p>
-        <Link to="/about/"><button>Learn More</button></Link>
+        <Link to="/about/"><button>More About Mina</button></Link>
       </p>
+
+      <hr />
+
+      <h3>Skills and Technologies</h3>
+      <div class="bar">
+        <span class="bar_content">
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Sass / SCSS</li>
+            <li>JavaScript</li>
+            <li>React.js</li>
+            <li>Gatsby.js</li>
+            <li>TypeScript</li>
+            <li>PHP</li>
+            <li>Visual Studio Code</li>
+            <li>GitHub</li>
+            <li>Git</li>
+            <li>Figma</li>
+            <li>Adobe Photoshop</li>
+            <li>Adobe Illustrator</li>
+            <li>Responsive UX/UI design</li>
+            <li>WordPress</li>
+            <li>Trello</li>
+            <li>SEO</li>
+            <li>Google Analytics</li>
+          </ul>
+        </span>
+      </div>
+
+      <hr />
+
+      <h3>Things I Like</h3>
+      <div class="venn-diagram">
+        <div class="venn-circle-1">
+          <p>Problem Solving</p>
+        </div>
+        <div class="venn-circle-2">
+          <p>Being Creative</p>
+        </div>
+        <div class="venn-middle">
+          <p>Web Design</p>
+        </div>
+      </div>
     </div>
 
     <div class="section-3">
-      <h3>My Projects</h3>
+      <h2>My Projects</h2>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi. architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione.
+        Check out a few of my projects, from my time at BCIT and beyond.
     </p>
       <p>
         <Link to="/projects/"><button>See All Projects</button></Link>
       </p>
+
+      <div class="s3-gallery">
+
+        <div class="s3-gallery-block1">
+          <StaticImage
+            src="../images/index_s1.jpg"
+            class="s3-gallery-img1"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Mina Bui's Portfolio Piece 1"
+          />
+          <p>
+            Project 1 <span>Jan 2021</span><br />
+            React.js movie database web application.
+          </p>
+        </div>
+
+        <div class="s3-gallery-block2">
+          <StaticImage
+            src="../images/index_s1.jpg"
+            class="s3-gallery-img2"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Mina Bui's Portfolio Piece 2"
+          />
+          <p>
+            Project 2 <span>March 2021</span><br />
+            WordPress Woocommerce website selling apparel.
+          </p>
+        </div>
+
+        <div class="s3-gallery-block3">
+          <StaticImage
+            src="../images/index_s1.jpg"
+            class="s3-gallery-img3"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Mina Bui's Portfolio Piece 3"
+          />
+          <p>
+            Project 3 <span>June 2021</span><br />
+            Gatsby.js and Typescript based portfolio site (that's where you are).
+          </p>
+        </div>
+      </div>
+
     </div>
 
   </Layout>
